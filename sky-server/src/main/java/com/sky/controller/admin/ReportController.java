049c63ef28ce387cda5915e1fce4235bf889b1dd
GITmaster <19175187219@163.com>
2024-11-08 19:33:40 +0800

    数据导出最终代码

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -56,5 +57,10 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end){
         log.info("销量前十，{},{}",begin,end);
         return Result.success(reportService.getTopTenStatistics(begin,end));
+    }
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        reportService.export(response);
+
     }
 }
